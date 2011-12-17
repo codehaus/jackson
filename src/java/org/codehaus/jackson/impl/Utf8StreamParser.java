519b6348e89a45360277691654dcf86e1a3618a1
cowtowncoder <cowtowncoder@cc757fca-8a48-0410-80b4-e22f7f27f4c6>
2011-12-17 05:11:06 +0000

    Merge [JACKSON-738] fix to 1.8

--- src/java/org/codehaus/jackson/impl/Utf8StreamParser.java
+++ src/java/org/codehaus/jackson/impl/Utf8StreamParser.java
@@ -1519,7 +1519,7 @@ public final class Utf8StreamParser
             if (c == INT_QUOTE) {
                 break main_loop;
             }
-
+            
             switch (codes[c]) {
             case 1: // backslash
                 _decodeEscaped();
@@ -2167,6 +2167,7 @@ public final class Utf8StreamParser
         if (_inputPtr >= _inputEnd) {
             loadMoreGuaranteed();
         }
+        d = (int) _inputBuffer[_inputPtr++];
         if ((d & 0xC0) != 0x080) {
             _reportInvalidOther(d & 0xFF, _inputPtr);
         }
