69893410e80640ffd981401b297cbc65ad2d3015
cowtowncoder <cowtowncoder@cc757fca-8a48-0410-80b4-e22f7f27f4c6>
2012-09-29 17:08:39 +0000

    Fix Smile [Issue#8]

--- branches/1.8/src/smile/java/org/codehaus/jackson/smile/SmileGenerator.java
+++ branches/1.8/src/smile/java/org/codehaus/jackson/smile/SmileGenerator.java
@@ -267,7 +267,7 @@ public class SmileGenerator
      */
     
     /**
-     * This <code>ThreadLocal</code> contains a {@link java.lang.ref.SoftRerefence}
+     * This <code>ThreadLocal</code> contains a {@link java.lang.ref.SoftReference}
      * to a buffer recycler used to provide a low-cost
      * buffer recycling for Smile-specific buffers.
      */
@@ -2049,10 +2049,12 @@ public class SmileGenerator
                 _seenNames = new SharedStringNode[MAX_SHARED_NAMES];
                 final int mask = MAX_SHARED_NAMES-1;
                 for (SharedStringNode node : old) {
-                    for (; node != null; node = node.next) {
+		    while (node != null) {
                         int ix = node.value.hashCode() & mask;
+			SharedStringNode next = node.next;
                         node.next = _seenNames[ix];
                         _seenNames[ix] = node;
+			node = next;
                     }
                 }
             }
