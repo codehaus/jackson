14ba34711d27d2083434615eb4e12f618d366b8d
cowtowncoder <cowtowncoder@cc757fca-8a48-0410-80b4-e22f7f27f4c6>
2012-11-16 02:52:20 +0000

    Backport fix for [Issue#42] (NPE for UTF-32 reader)

--- trunk/src/test/org/codehaus/jackson/impl/TestJsonParser.java
+++ trunk/src/test/org/codehaus/jackson/impl/TestJsonParser.java
@@ -411,6 +411,7 @@ public class TestJsonParser
             // should still know the field name
             assertEquals("doc", jp.getCurrentName());
             assertToken(JsonToken.END_OBJECT, jp.nextToken());
+            assertNull(jp.nextToken());
             jp.close();
         }
     }
